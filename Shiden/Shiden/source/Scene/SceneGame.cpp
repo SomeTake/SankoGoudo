f739120c4e1510dac52fcb8a8be5c053ab4a5dc7
SomeTake <tp1002217g@gmail.com>
2020-01-07 14:16:26 +0900

    コメント追加

--- Shiden/Shiden/source/Scene/SceneGame.cpp
+++ Shiden/Shiden/source/Scene/SceneGame.cpp
@@ -1,10 +1,22 @@
+//=============================================================================
+//
+// シーンゲーム[SceneGame.cpp]
+// Author : GP12B332 19 染谷武志
+//
+//=============================================================================
 #include "SceneGame.h"
 #include "../Player/PlayerController.h"
 
+//==========================================
+// 入場
+//==========================================
 void SceneManager::SceneGame::OnStart(SceneManager & entity)
 {
 }
 
+//==========================================
+// 更新
+//==========================================
 SceneManager::State SceneManager::SceneGame::OnUpdate(SceneManager & entity)
 {
 	// インスタンスの更新
@@ -13,6 +25,9 @@ SceneManager::State SceneManager::SceneGame::OnUpdate(SceneManager & entity)
 	return State::Game;
 }
 
+//==========================================
+// 描画
+//==========================================
 void SceneManager::SceneGame::OnDraw(SceneManager & entity)
 {
 	// フィールドの描画
--- Shiden/Shiden/source/Scene/SceneResult.cpp
+++ Shiden/Shiden/source/Scene/SceneResult.cpp
@@ -1,14 +1,29 @@
+//=============================================================================
+//
+// シーンリザルト[SceneResult.cpp]
+// Author : GP12B332 19 染谷武志
+//
+//=============================================================================
 #include "SceneResult.h"
 
+//==========================================
+// 入場
+//==========================================
 void SceneManager::SceneResult::OnStart(SceneManager & entity)
 {
 }
 
+//==========================================
+// 更新
+//==========================================
 SceneManager::State SceneManager::SceneResult::OnUpdate(SceneManager & entity)
 {
 	return State::Result;
 }
 
+//==========================================
+// 描画
+//==========================================
 void SceneManager::SceneResult::OnDraw(SceneManager & entity)
 {
 }
